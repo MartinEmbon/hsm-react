5ec7447af669b8c2f455596a5af9eeea8ba4d350
Martincho <martinembon@hotmail.com>
2021-11-19 17:36:40 -0300

    updated exercies

---
+++ gama.js
@@ -0,0 +1,49 @@
+// Exercicio Cadastro de Artista
+
+function cadastrarArtista(x,y,z){
+    let listaArtistas = []
+    listaArtistas.push(x,y,z)    
+    console.log(listaArtistas) 
+}
+
+cadastrarArtista("Maria Rita","MPB",["sabado","domingo"])
+cadastrarArtista("Emicida","Rap",["sexta","sabado"])
+
+// Exercicio Artistas Disponíveis
+
+let listaArtistas = [
+    {
+        nome:"Maria Rita",
+        estiloMusical:"MPB",
+        diasDisponiveis:["sábado","domingo"]
+    },
+    {
+        nome:"Emicida",
+        estiloMusical:"Rap",
+        diasDisponiveis:["sexta","sábado"]
+    },
+
+    {
+        nome:"Liniker",
+        estiloMusical:"Soul",
+        diasDisponiveis:["quinta-feira","domingo"]
+    },
+
+    {
+        nome:"Duda Beat",
+        estiloMusical:"Pop",
+        diasDisponiveis:["quarta-feira","sexta-feira"]
+    }    
+]
+
+
+function artistasDisponiveis(diaSemana){    
+    for (let i = 0;i<=listaArtistas.length;i++){
+        const artistaDia = listaArtistas.filter((artista=>{           
+            if(diaSemana === artista.diasDisponiveis[i]){
+                console.log(`Artista: ${artista.nome} - Estilo: ${artista.estiloMusical}`)
+            }        
+        }))        
+    }    
+}
+artistasDisponiveis("domingo")
